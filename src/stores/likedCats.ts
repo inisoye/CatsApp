4ffdf92851da9cf428554eebf98a34fe8ddf5a44
Inioluwa Akinyosoye <inisoye@gmail.com>
2022-09-25 23:47:24 +0100

    feat: add persistence to store

--- src/stores/likedCats.ts
+++ src/stores/likedCats.ts
@@ -1,28 +1,53 @@
 import create from 'zustand';
+import { persist, type StateStorage } from 'zustand/middleware';
+import { MMKV } from 'react-native-mmkv';
 
-export interface LikedCat {
+const storage = new MMKV();
+
+const mmkvStorage: StateStorage = {
+  setItem: (name, value) => {
+    return storage.set(name, value);
+  },
+  getItem: name => {
+    const value = storage.getString(name);
+    return value ?? null;
+  },
+  removeItem: name => {
+    return storage.delete(name);
+  },
+};
+
+interface LikedCat {
   id: string | undefined;
   name: string | undefined;
   uri: string | undefined;
 }
 
-export interface LikedCatsStore {
+interface LikedCatsStore {
   likedCats: LikedCat[];
   toggleCat: (cat: LikedCat, isCatLiked: boolean) => void;
 }
 
-const useLikedCatsStore = create<LikedCatsStore>(set => ({
-  likedCats: [],
+const useLikedCatsStore = create<LikedCatsStore>()(
+  persist(
+    set => ({
+      likedCats: [],
 
-  toggleCat: (toggledCat, isCatLiked) =>
-    set(state => {
-      return {
-        likedCats: isCatLiked
-          ? state.likedCats.filter(cat => cat.id !== toggledCat.id) // New state with liked cat
-          : [...state.likedCats, { ...toggledCat }], // New state without unliked cat
-      };
+      toggleCat: (toggledCat, isCatLiked) =>
+        set(state => {
+          return {
+            likedCats: isCatLiked
+              ? state.likedCats.filter(cat => cat.id !== toggledCat.id) // New state with liked cat
+              : [...state.likedCats, { ...toggledCat }], // New state without unliked cat
+          };
+        }),
     }),
-}));
+    {
+      name: 'likedCatsStore',
+      getStorage: () => mmkvStorage,
+    },
+  ),
+);
 
 const toggleCatSelector = (state: LikedCatsStore) => state.toggleCat;
 const likedCatsSelector = (state: LikedCatsStore) => state.likedCats;

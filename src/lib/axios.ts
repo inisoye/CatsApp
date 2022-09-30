7d948209c1b8a9574adf4976c5f4af8fe414d3a1
Inioluwa Akinyosoye <inisoye@gmail.com>
2022-09-30 11:08:13 +0100

    fix: replace apisauce with axios to prevent bugs

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,9 +1,13 @@
-import { create } from 'apisauce';
+import axios from 'axios';
 import Config from 'react-native-config';
 
 const { API_BASE_URL, CAT_API_KEY } = Config;
 
-export const catApiClient = create({
+export const catApiClient = axios.create({
   baseURL: API_BASE_URL,
   headers: { 'x-api-key': CAT_API_KEY },
+});
+
+export const testCatApiClient = axios.create({
+  baseURL: API_BASE_URL,
 });

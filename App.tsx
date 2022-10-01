149af141f1282af5e995124ec906669894b5e62e
Inioluwa Akinyosoye <inisoye@gmail.com>
2022-10-01 18:19:46 +0100

    feat: add splash screen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
 import * as React from 'react';
+import RNBootSplash from 'react-native-bootsplash';
 
 import { AppProviders } from 'providers';
 import { AppScreens } from 'lib';
 import { StatusBar } from 'components';
+import { getBreeds } from 'api';
 
 const App = () => {
+  React.useEffect(() => {
+    /*  */
+    const init = async () => {
+      await getBreeds();
+    };
+
+    init().finally(async () => {
+      await RNBootSplash.hide({ fade: true });
+    });
+  }, []);
+
   return (
     <AppProviders>
       <StatusBar />
